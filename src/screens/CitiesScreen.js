0656f90131e02b7ba3ebbbf0c81e9929e06e81a3
juanmoyano123 <76905472+juanmoyano123@users.noreply.github.com>
2022-09-28 13:23:48 -0300

    renderizado de city cards

--- src/screens/CitiesScreen.js
+++ src/screens/CitiesScreen.js
@@ -1,15 +1,41 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, StyleSheet , Image } from 'react-native'
+import { useGetAllCitiesQuery } from '../features/citiesAPI';
+import CitiesCards from '../components/CitiesCards';
 import React from 'react'
 
-export default function CitiesScreen() {
-    return (
-        <View>
-            <Text>CitiesScreen</Text>
-            <Text>CitiesScreen</Text>
-            <Text>CitiesScreen</Text>
-            <Text>CitiesScreen</Text>
-            <Text>CitiesScreen</Text>
-            <Text>CitiesScreen</Text>
-        </View>
-    )
+export default function CityScreen() {
+
+  const {data: items} = useGetAllCitiesQuery()
+  
+  console.log("holaaa")
+  console.log(items)
+  return (
+    <View>
+      
+    < FlatList
+        data={items}
+        renderItem={({item})=>(
+          <CitiesCards>
+            <Text> {item?.city} </Text>
+              <Image
+                source={{uri:item?.photo}}
+                style={styles.images}
+            />  
+             <Text> {item.description} </Text>
+
+          </CitiesCards>
+        )}
+    />
+
+    </View>
+  )
 }
+
+
+const styles = StyleSheet.create({
+
+  images: {
+    width: 300,
+  },
+ 
+})
