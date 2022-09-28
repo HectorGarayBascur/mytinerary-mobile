3a8f120ed0834fc7d730d45101a9803ba1b35a23
Hector Garay <hectorgaraybascur@gmail.com>
2022-09-28 12:47:58 -0300

    screens app

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,29 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import Video from '../components/Video'
+
+
+export default function HomeScreen() {
+    return (
+        <View>
+            <Text style={styles.welcome}>Welcome to Mytineraries</Text>
+            <Video />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    welcome: {
+        width: 430,
+        height: 50,
+        paddingTop: 5,
+        textAlign: 'center',
+        fontSize: 40,
+        color: '#fff',
+        backgroundColor: '#73B1BF'
+    },
+    // container: {
+    //     flex: 1,
+    //     backgroundColor: 'white',
+    // }
+})
