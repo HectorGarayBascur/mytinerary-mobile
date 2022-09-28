0ed349d9ac8f71e634606de78dccf9221dd315f8
juanmoyano123 <76905472+juanmoyano123@users.noreply.github.com>
2022-09-28 13:50:53 -0300

    fix rutas api

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import {Provider} from "react-redux"
+import store from "./src/features/store"
 import { View } from 'react-native';
 import Drawer from './src/navigation/Drawer';
 
 export default function App() {
   return (
+    <Provider store={store}>
     <NavigationContainer>
       <Drawer></Drawer>
     </NavigationContainer>
+    </Provider> 
   );
 }
-
 
