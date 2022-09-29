ed03c5196186cf02bc7df74d657aad0e1f4f2eed
francoEmunoz <munozfranco011@gmail.com>
2022-09-29 17:33:43 -0300

    ruta SignUp

--- src/navigation/Drawer.js
+++ src/navigation/Drawer.js
@@ -3,6 +3,7 @@ import React from 'react'
 import HomeScreen from '../screens/HomeScreen';
 import CitiesScreen from '../screens/CitiesScreen';
 import CityScreen from '../screens/CityScreen'
+import SignUp from '../screens/SignUp';
 
 
 const DrawerNavigation = createDrawerNavigator();
@@ -13,6 +14,7 @@ export default function Drawer() {
             <DrawerNavigation.Screen name='Home' component={HomeScreen} options={{ headerBackTitleVisible: false }}></DrawerNavigation.Screen>
             <DrawerNavigation.Screen name='Cities' component={CitiesScreen}></DrawerNavigation.Screen>
             <DrawerNavigation.Screen name='City' component={CityScreen}></DrawerNavigation.Screen>
+            <DrawerNavigation.Screen name='SignUp' component={SignUp}></DrawerNavigation.Screen>
         </DrawerNavigation.Navigator>
     )
 }
