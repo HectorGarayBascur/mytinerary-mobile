d6c3a5f0cb68dc7497e6a0d7ba44658c9c675073
juanmoyano123 <76905472+juanmoyano123@users.noreply.github.com>
2022-09-30 10:55:29 -0300

    filtro en funcionamiento

--- src/screens/CitiesScreen.js
+++ src/screens/CitiesScreen.js
@@ -2,20 +2,28 @@ import { View,   StyleSheet, Image, TextInput, Text } from 'react-native'
 import { useGetAllCitiesQuery } from '../features/citiesAPI';
 import CitiesCards from '../components/CitiesCards';
 import React from 'react'
+import { useState } from 'react'
+
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function CityScreen() {
-
+  const [input, setInput] = useState()
   const { data: items } = useGetAllCitiesQuery()
-  console.log("holaaa")
-  console.log(items)
+
+  function upperCaseOne(value){
+    return value.charAt(0).toUpperCase() + value.slice(1)
+  }
+
+  const filteredData = items?.filter(item => item?.city?.includes(input))
+
+console.log("holaaa")
+console.log(filteredData)
+
   return (
     <View>
-
-      <TextInput placeholder={"Enter your city..."} style={styles.input} />
-      <CitiesCards cities={items} />
-   
-
+      <TextInput placeholder={"Enter your city..."} style={styles.input} onChangeText={(value)=> setInput(upperCaseOne(value))} />
+      <CitiesCards cities={(filteredData.length>0)? filteredData: items} />
+  
     </View>
   )
 }
